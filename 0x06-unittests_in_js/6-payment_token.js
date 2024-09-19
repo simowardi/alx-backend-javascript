e8a4e880cb76e900d521a3b1275babfcb45d918e
simowardi <daoist32@gmail.com>
2024-09-19 13:37:57 +0000

    unittests_in_js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
+  }
+});
+
+module.exports = getPaymentTokenFromAPI;
