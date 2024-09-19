82a80fc87e18930feffa1c7486da736fb7311ba5
simowardi <daoist32@gmail.com>
2024-09-19 12:35:18 +0000

    update task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,13 @@
-/**
- * 1-stdin.js - Interactive program to get user's name and display messages
- *
- * This program prompts the user for their name, displays it, and shows a
- * closing message when the program ends. It uses process.stdin and process.stdout
- * for input and output operations.
-**/
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (msg) => {
-    process.stdout.write(`Your name is: ${msg.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (msg) => {
-    process.stdout.write(`Your name is: ${msg.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.on('readable', () => {
+  const msg = process.stdin.read();
+
+  if (msg) {
+    process.stdout.write(`Your name is: ${msg}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
